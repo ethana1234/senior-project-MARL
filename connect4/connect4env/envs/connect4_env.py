31afb7493b6f44f8fc803045beb1bed216526dd8
Ethan Agranoff <ethana1234@gmail.com>
2020-04-20 14:37:44 -0600

    Fixed winner

--- connect4/connect4env/envs/connect4_env.py
+++ connect4/connect4env/envs/connect4_env.py
@@ -7,8 +7,8 @@ TOTAL_BOARD_SPACES = BOARD_ROWS*BOARD_COLS
 COORD_TO_INDEX = lambda x : (x[0] * BOARD_ROWS) + x[1]
 
 
-# Class that implements the Gym Environment Interface
 class Connect4Env(gym.Env):
+    '''Class that implements the Gym Environment Interface'''
     metadata = {'render.modes': ['human']}
     
     def __init__(self):
@@ -54,8 +54,8 @@ class Connect4Env(gym.Env):
             print(out)
         print('-------------')
         
-    # Update vacant positions after a turn is made
     def availablePositions(self):
+        '''Update vacant positions after a turn is made'''
         positions = []
         for i in range(BOARD_ROWS):
             for j in range(BOARD_COLS):
@@ -64,68 +64,68 @@ class Connect4Env(gym.Env):
                     positions.append((i,j))
         return positions
         
-    # Get a unique hash value that corresponds with the current board state
-    # This is used to store the board state in a state-value dictionary
     def getHash(self):
+        '''Get a unique hash value that corresponds with the current board state
+        This is used to store the board state in a state-value dictionary'''
         return str(self.board.reshape(BOARD_ROWS * BOARD_COLS))
+
+    def find_runs(self, x):
+        """Find runs of consecutive items in an array.
+        Credit to Github user alimanfoo for this function"""
+
+        # ensure array
+        x = np.asanyarray(x)
+        if x.ndim != 1:
+            raise ValueError('only 1D array supported')
+        n = x.shape[0]
+
+        # handle empty array
+        if n == 0:
+            return np.array([]), np.array([]), np.array([])
+
+        else:
+            # find run starts
+            loc_run_start = np.empty(n, dtype=bool)
+            loc_run_start[0] = True
+            np.not_equal(x[:-1], x[1:], out=loc_run_start[1:])
+            run_starts = np.nonzero(loc_run_start)[0]
+
+            # find run values
+            run_values = x[loc_run_start]
+
+            # find run lengths
+            run_lengths = np.diff(np.append(run_starts, n))
+
+            # Now find if a winner exists
+            for ind,val in enumerate(run_values):
+                if val == 1 and run_lengths[ind] >= 4:
+                    return (1,0)
+                elif val == -1 and run_lengths[ind] >= 4:
+                    return (0,1)
+
+            return None
         
-    # After each move, check if there's a winner and give out rewards
-    def winner(self, row, column):
-        # Col win
-        # p1 wins
-        if sum(self.board[row:min(BOARD_COLS,row+4),column]) == 4:
-            return (1, 0)
-        # p2 wins
-        elif sum(self.board[row:min(BOARD_COLS,row+4), column]) == -4:
-            return (0, 1)
+    def winner(self, row, col):
+        '''After each move, check if there's a winner and give out rewards'''
+        row_win = self.find_runs(self.board[row,:])
+        if row_win is not None:
+            return row_win
 
-        for i in range(4):
-        # Row win 
-            if sum(self.board[row, max(0,column-i):min(BOARD_ROWS,column+4-i)]) == 4:
-                return (1, 0)
-            elif sum(self.board[row, max(0,column-i):min(BOARD_ROWS, column+4-i)]) == -4:
-                return (0, 1)
+        col_win = self.find_runs(self.board[:,col])
+        if col_win is not None:
+            return col_win
 
-        diag = self.board.diagonal(offset = column-row)
-
-        if len(diag) >= 4:
-            for i in range(len(diag)-3):
-                if sum(diag[i:i+4]) == 4:
-                    return (1, 0)
-                if sum(diag[i:i+4]) == -4:
-                    return (0, 1)
-                if sum(diag[i-4:i]) == 4:
-                    return (1, 0)
-                if sum(diag[i-4:i]) == -4:
-                    return (0, 1)
-
-#TODO
-#failure example test case
-#a=np.array([0,0,0,0,0,0,0]*6)
-#a[4][0]=1
-#a[3][1]=1
-#a[2][2]=1
-#a[1][3]=1
-#winner(a, 4,0)
-
-        diag2 = np.fliplr(self.board).diagonal(offset = row-column)
-        if len(diag2) >= 4:
-            for i in range(len(diag2)-3):
-                if sum(diag2[i:i+4]) == 4:
-                    return (1, 0)
-                if sum(diag2[i:i+4]) == -4:
-                    return (0, 1)
-                if sum(diag2[i-4:i]) == 4:
-                    return (1, 0)
-                if sum(diag2[i-4:i]) == -4:
-                    return (0, 1)
+        diag_win = self.find_runs(self.board.diagonal(offset=row-col+3))
+        if diag_win is not None:
+            return diag_win
+        diag_win = self.find_runs(np.fliplr(self.board).diagonal(offset=row+col-4))
+        if diag_win is not None:
+            return diag_win
                     
         
         # Tie
         if not len(self.availablePositions()):
-            self.gameOver = True
             return (0.1, 0.5)
         
         # Game not over
         return None
-    
