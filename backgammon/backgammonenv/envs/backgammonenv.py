ce58f52f11e15210ab7239b28b5022193c8e5161
Sarah <sarahbae1998@gmail.com>
2020-06-01 12:49:26 -0700

    work on backgammon env, need to add some more rules

--- backgammon/backgammonenv/envs/backgammonenv.py
+++ backgammon/backgammonenv/envs/backgammonenv.py
@@ -2,13 +2,10 @@ import numpy as np
 import gym
 from gym import spaces
 
-TOTAL_BOARD_PTS = 26 #first and last are home (end states)
-P1_HOME = 0
-P2_HOME = 25
-P1_CHECKERS = 0
-P2_CHECKERS = 1
+TOT_BOARD_PTS = 24
+P1 = 0
+P2 = 1
 ALL_CHECKERS = 15
-
 
 class BackgammonEnv(gym.Env):
     '''Class that implements the Gym Environment Interface'''
@@ -17,67 +14,106 @@ class BackgammonEnv(gym.Env):
     def __init__(self):
         super(BackgammonEnv, self).__init__()
         
-        self.action_space = spaces.Discrete(TOTAL_BOARD_PTS)
-        self.observation_space = spaces.Discrete(TOTAL_BOARD_PTS)
-        self.board = np.array([ [0,0],
-                                [0,2], [0,0], [0,0], [0,0], [0,0], [5,0],
-                                [0,0], [3,0], [0,0], [0,0], [0,0], [0,5],
-                                [5,0], [0,0], [0,0], [0,0], [0,3], [0,0],
-                                [0,5], [0,0], [0,0], [0,0], [0,0], [2,0],
-                                [0,0]] )
+        self.action_space = spaces.Discrete(TOT_BOARD_PTS)
+        self.observation_space = spaces.Discrete(TOT_BOARD_PTS)
+        self.homePts = {P1: [i for i in range(18,24)], P2: [i for i in range(6)]}
+        self.board = np.array([(0, None)] * TOT_BOARD_PTS)
+        self.board[0] = (2, P1)
+        self.board[11] = (5, P1)
+        self.board[16] = (3, P1)
+        self.board[18] = (5, P1)
+        self.board[5] = (5, P2)
+        self.board[7] = (3, P2)
+        self.board[12] = (5, P2)
+        self.board[23] = (2, P2)
+        self.out = [0,0]
         self.gameOver = False
-        self.playerTurn = 0
+        self.homePts = { P2: [18,19,20,21,22,23], P1: [0,1,2,3,4,5] }
+        self.playerTurn = P1
         self.roll = (np.random.randint(1,7), np.random.randint(1,7))
     
     def reset(self):
-        self.board = np.array([ [0,0],
-                                [0,2], [0,0], [0,0], [0,0], [0,0], [5,0],
-                                [0,0], [3,0], [0,0], [0,0], [0,0], [0,5],
-                                [5,0], [0,0], [0,0], [0,0], [0,3], [0,0],
-                                [0,5], [0,0], [0,0], [0,0], [0,0], [2,0],
-                                [0,0]] )
+        self.board = np.array([(0, None)] * TOT_BOARD_PTS)
+        self.board[0] = (2, P1)
+        self.board[11] = (5, P1)
+        self.board[16] = (3, P1)
+        self.board[18] = (5, P1)
+        self.board[5] = (5, P2)
+        self.board[7] = (3, P2)
+        self.board[12] = (5, P2)
+        self.board[23] = (2, P2)
+        self.out = [0,0]
         self.gameOver = False
-        self.playerTurn = 0
+        self.playerTurn = P1
         self.roll = (np.random.randint(1,7), np.random.randint(1,7))
         
     def step(self, action):
         reward = self.winner(action)
         if reward is not None:
             self.gameOver = True
-        self.playerTurn = 1 if self.playerTurn == 0 else 0
+        self.playerTurn = P2 if self.playerTurn == P1 else P1
         return self.board, reward, self.gameOver, self.getHash()
     
     def render(self):
         print(self.board)
-   '''     
-   to be done outside of env
-    def useRoll(self, roll):
-        possibleMoves = np.array([])
-        opponent = 1 - self.playerTurn
-        for i in range(1, TOTAL_BOARD_PTS - 1):
-            if board[i][self.playerTurn] > 0:
-                if board[roll+i][opponent] < 2:
-                    #validMove
-                    move1 = (i,roll+i)
-                    #find any possible next moves
-                    for i in range (1, TOTAL_BOARD_PTS - 1):
-
         
     def getValidMoves(self):
         '''roll and get valid moves from board'''
+        
+        possibleMoves = np.array([])
         self.roll = (np.random.randint(1,7), np.random.randint(1,7))
-        move1 = useRoll(self.roll[0])
-        move2 = useRoll(self.roll[1])
+        move1 = useRoll(self.roll[0]) #this is an array of possible from/to moves for roll 0
+        move2 = useRoll(self.roll[1]) #array of possible from/to moves for roll 1
+        moveComb = useRoll(self.roll[0] + self.roll[1]) #array of possible from/to moves using both rolls at once
         if self.roll[0]==self.roll[1]
             move3 = useRoll(self.roll[0])
             move4 = useRoll(self.roll[1])
+            possibleMoves.append(np.array([move1,move2,move3,move4]))
+        else
+            possibleMoves.append(np.array([move1,move2]))
+        return possibleMoves
+
+
+    def bearOff(self):
+        tot = sum(np.array([self.board[position][0] for position in self.homePts[self.playerTurn] if self.playerTurn == self.board[position][1]]))
+        return tot == (ALL_CHECKERS - self.out[self.playerTurn])
+
 '''
+    def bearOffOneRoll(self, roll):
+        # use one roll to move the only checkers outside home board to home board, bear off with other roll(s)
+        tot = sum(np.array([self.board[position][0] for position in self.homePts[self.playerTurn] if self.playerTurn == self.board[position][1]]))
+        threshold = ALL_CHECKERS - (len(roll)-1)
+        return tot >= (threshold - self.out[self.playerTurn])
+
+    def canMoveTo(self, target):
+        if target < 0 or target > 23:
+            return self.bearOff()
+        return self.board[target][0] < 2 or (self.board[target][0] > 1 and self.board[target][1] == self.playerTurn)
+
+    def isValid(self, target):
+        if 0 <= target < TOT_BOARD_PTS:
+            return self.board[target][0] < 2 or (self.board[target][0] > 1 and self.board[target][1] == self.playerTurn)
+        return False
+'''
+
+    def useRoll(self, roll):
+        opponent = 1 - self.playerTurn
+        possibleMove = np.array([])
+        for i in range(TOT_BOARD_PTS):
+            if board[i][0] > 0 and board[i][1] == self.playerTurn:
+                target = roll + i
+                if target < 0 or target > 23:
+                    return self.bearOff()
+                if board[target][0] < 2 or self.board[target][1] != opponent:
+                    #validMove
+                    possibleMove.append((i,target))
+        return possibleMove
 
         
     def getHash(self):
         '''Get a unique hash value that corresponds with the current board state
         This is used to store the board state in a state-value dictionary'''
-        return str(self.board.reshape(TOTAL_BOARD_PTS))
+        return str(self.board.reshape(TOT_BOARD_PTS))
         
     def winner(self):
         '''After each move, check if there's a winner and give out rewards'''
